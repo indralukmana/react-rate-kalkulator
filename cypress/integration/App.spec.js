7a19496be6948cb3d6fc3a6fbb4dbf0bce5a0739
Indra Lukmana <indralukmana@live.com>
2019-06-14 00:09:54 +0700

    Add first feature
    Worked on the hourly rate to weekly rate conversion

---
+++ cypress/integration/App.spec.js
@@ -0,0 +1,24 @@
+// type definitions for Cypress object "cy"
+/// <reference types="cypress" />
+
+describe('Test the kalkulator', () => {
+	it('Should visit the site', () => {
+		cy.visit('/');
+
+		cy.get('input[data-testid=hourly-rate]').should('have.value', '10');
+		cy.get('input[data-testid=work-hours').should('have.value', '40');
+		cy.get('input[data-testid=weekly-rate]').should('have.value', '400');
+
+		cy.get('input[data-testid=hourly-rate]')
+			.should('have.value', '10')
+			.clear()
+			.type('15')
+			.should('have.value', '15');
+		cy.get('input[data-testid=weekly-rate]')
+			.should('have.value', '600')
+			.clear()
+			.type(300);
+
+		cy.get('input[data-testid=hourly-rate]').should('have.value', '7.5');
+	});
+});
--- src/App.js
+++ src/App.js
@@ -1,26 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import {
+	CssBaseline,
+	Container,
+	Typography,
+	TextField
+} from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+	paper: {
+		marginTop: theme.spacing(8), // = 4 * 2
+		display: 'flex',
+		flexDirection: 'column',
+		alignItems: 'center'
+	}
+}));
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const classes = useStyles();
+	const [hourlyRate, setHourlyRate] = useState(10);
+	const [workHour, setWorkHour] = useState(40);
+	const [weeklyRate, setWeeklyRate] = useState(400);
+
+	const tryConvert = ({ name, value }) => {
+		if (name === 'hourly-rate') {
+			setHourlyRate(value);
+			setWeeklyRate(value * workHour);
+		} else if (name === 'weekly-rate') {
+			setHourlyRate(value / workHour);
+			setWeeklyRate(value);
+		}
+	};
+
+	return (
+		<Container component="main" maxWidth="xs">
+			<CssBaseline />
+			<div className={classes.paper}>
+				<Typography component="h1" variant="h5">
+					Rate Kalkulator
+				</Typography>
+				<TextField
+					variant="outlined"
+					margin="normal"
+					id="hourly-rate"
+					label="Hourly Rate"
+					name="hourly-rate"
+					autoFocus
+					inputProps={{ 'data-testid': 'hourly-rate' }}
+					value={hourlyRate}
+					onChange={event => tryConvert(event.target)}
+				/>
+				<TextField
+					variant="outlined"
+					margin="normal"
+					id="work-hours"
+					label="Work Hours"
+					name="work-hours"
+					inputProps={{ 'data-testid': 'work-hours' }}
+					value={workHour}
+					onChange={event => setWorkHour(event.target.value)}
+				/>
+				<TextField
+					variant="outlined"
+					margin="normal"
+					id="weekly-rate"
+					label="Weekly Rate"
+					name="weekly-rate"
+					autoFocus
+					inputProps={{ 'data-testid': 'weekly-rate' }}
+					value={weeklyRate}
+					onChange={event => tryConvert(event.target)}
+				/>
+			</div>
+		</Container>
+	);
 }
 
 export default App;
